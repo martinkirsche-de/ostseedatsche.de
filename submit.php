80d45ee0ccd083c9f04b18589affd575ab196d5f
martinkirsche-de <admin@martinkirsche.de>
2024-04-05 22:56:01 +0200

    Improve messaging script

--- submit.php
+++ submit.php
@@ -6,6 +6,23 @@ function get($s, $d = ""): string
   return empty($v) ? $d : $v;
 }
 
+function get_date($s): string
+{
+  $value = get($s);
+  if(!$value)
+  {
+    return "[keine Angabe]";
+  }
+  try {
+    $date = DateTime::createFromFormat("Y-m-d", $value);
+    if($date) {
+      return $date->format("d.m.Y");
+    }
+  } catch (Exception $e) {
+  }
+  return $value;
+}
+
 $name = get("vorname", "[keine Angabe]")." ".get("nachname", "[keine Angabe]");
 $email = get("email", "[keine Angabe]");
 $bungalow = get("bungalow");
@@ -14,22 +31,25 @@ $strasse = get("strasse", "[keine Angabe]");
 $postleitzahl = get("postleitzahl", "[keine Angabe]");
 $ort = get("ort", "[keine Angabe]");
 $personen = get("personen", "[keine Angabe]");
-$von = get("von", "[keine Angabe]");
-$bis = get("bis", "[keine Angabe]");
+$von = get_date("von");
+$bis = get_date("bis");
 $nachricht = get("nachricht");
+$spam = get("checkbox") == "yes" || get("uncheckbox") != "yes";
 
-if(empty($bungalow))
+if(!$spam)
 {
-  $subject = "Nachricht von ".$name;
-  $body = $name." (".$email." / ".$telefon.") hat eine Nachricht geschickt.
+  if(empty($bungalow))
+  {
+    $subject = "Nachricht von ".$name;
+    $body = $name." (".$email." / ".$telefon.") hat eine Nachricht geschickt.
 
 Nachricht:
-".$nachricht;  
-} 
-else
-{
-  $subject = "Anfrage von ".$name;
-  $body = $name." (".$email.") hat eine Anfrage für den
+".$nachricht;
+  }
+  else
+  {
+    $subject = "Anfrage von ".$name;
+    $body = $name." (".$email.") hat eine Anfrage für den
 Zeitraum vom ".$von." bis ".$bis." im ".$bungalow." (für ".$personen." Personen) geschickt.
 
 Adresse:
@@ -41,15 +61,16 @@ Telefon:
 
 Nachricht:
 ".$nachricht;
+  }
+
+  $to = "info@ostseedatsche.de";
+  $headers = "From: info@ostseedatsche.de" . "\r\n" .
+      "Reply-To: ". $email ."\r\n" .
+      "X-Mailer: PHP/" . phpversion() ."\r\n";
+
+  mail($to, $subject, $body, $headers);
 }
-
-$to = "info@ostseedatsche.de";
-$headers = "From: info@ostseedatsche.de" . "\r\n" .
-    "Reply-To: ". $email ."\r\n" .
-    "X-Mailer: PHP/" . phpversion();
-
-mail($to, $subject, $body, $headers);
 
 header("Location: https://www.ostseedatsche.de/gesendet");
 exit();
-?> 
+?>
